dc6f85cb09a5cf254fae18932d24c135a0a3bd11
Zihan Zhang <hblues419@gmail.com>
2022-11-14 00:11:40 +0000

    feat: update siteHeader component in src folder

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import HomePage from "./pages/HomePage";
 import PopularMoviePage from './pages/Movie/PopularMoviePage';
 import UpcomingMoviePage from './pages/Movie/UpcomingMoviePage';
 import TopRatedMoviePage from './pages/Movie/TopRatedMoviePage';
-import DetailPage from './pages/Detail/DetailPage';
+import PopularTVPage from './pages/TV/PopularTVPage';
+import PopularPeoplePage from './pages/People/PopularPeoplePage';
+import ItemDetailPage from './pages/Detail/ItemDetailPage';
+import PeopleDetailPage from './pages/Detail/PeopleDetailPage';
+import MyFavorite from './pages/Account/MyFavorite';
 import MenuContextProvider from "./contexts/menuContext";
 import MoviesContextProvider from "./contexts/moviesContext";
 
@@ -31,16 +35,15 @@ const App = () => {
           <MoviesContextProvider>
             <Routes>
               <Route path="/" element={<HomePage />} />
-              <Route path="/movies" element={<PopularMoviePage />} />
-              <Route path="/movies/:id" element={<DetailPage type='movie' />} />
-              <Route path="/movies/upcoming" element={<UpcomingMoviePage />} />
-              <Route path="/movies/top-rated" element={<TopRatedMoviePage />} />
-              <Route path="/tv" element={<HomePage />} />
-              <Route path="/tv/:id" element={<HomePage />} />
-              <Route path="/people" element={<HomePage />} />
-              <Route path="/people/:id" element={<HomePage />} />
-              <Route path="/:user/favourites" element={<HomePage />} />
-              <Route path="/:user/watchlist" element={<HomePage />} />
+              <Route path="/movie" element={<PopularMoviePage />} />
+              <Route path="/movie/:id" element={<ItemDetailPage type='movie' />} />
+              <Route path="/movie/upcoming" element={<UpcomingMoviePage />} />
+              <Route path="/movie/top-rated" element={<TopRatedMoviePage />} />
+              <Route path="/tv" element={<PopularTVPage />} />
+              <Route path="/tv/:id" element={<ItemDetailPage type='tv' />} />
+              <Route path="/people" element={<PopularPeoplePage />} />
+              <Route path="/people/:id" element={<PeopleDetailPage />} />
+              <Route path="/account/favourite" element={<MyFavorite />} />
               <Route path="*" element={<Navigate to="/" />} />
             </Routes>
           </MoviesContextProvider>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import MovieBackdrop from '../components/movieBackdrop';
 import MovieList from '../components/movieList';
 import Spinner from '../components/spinner';
-import {
-  getPopularMovies,
-  getTopRatedMovies,
-  getUpcoming,
-  getPopularTV,
-} from '../api/tmdbApi';
+import { getPopularMovies, getTopRatedMovies, getUpcoming, getPopularTV } from '../api/tmdbApi';
 import { useQueries } from 'react-query';
 import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const results = useQueries([
-    { queryKey: ['popularMovies', 1], queryFn: getPopularMovies },
-    { queryKey: ['topRatedMovies'], queryFn: getTopRatedMovies },
-    { queryKey: ['upComingMovies'], queryFn: getUpcoming },
-    { queryKey: ['popularTV'], queryFn: getPopularTV },
+    { queryKey: ['popularMovies1', { page: 1 }], queryFn: getPopularMovies },
+    { queryKey: ['topRatedMovies1', { page: 1 }], queryFn: getTopRatedMovies },
+    { queryKey: ['upComingMovies1', { page: 1 }], queryFn: getUpcoming },
+    { queryKey: ['popularTV1', { page: 1 }], queryFn: getPopularTV },
   ]);
 
   if (
@@ -29,12 +24,7 @@ const HomePage = () => {
     return <Spinner />;
   }
 
-  if (
-    results[0].isError ||
-    results[1].isError ||
-    results[2].isError ||
-    results[3].isError
-  ) {
+  if (results[0].isError || results[1].isError || results[2].isError || results[3].isError) {
     return <h1>Error in fetching data</h1>;
   }
 
@@ -61,11 +51,7 @@ const HomePage = () => {
   return (
     <>
       <MovieBackdrop />
-      <Typography
-        gutterBottom
-        variant="h2"
-        component="p"
-        sx={{ textAlign: 'center' }}>
+      <Typography gutterBottom variant="h2" component="p" sx={{ textAlign: 'center' }}>
         Find you like
       </Typography>
 
@@ -74,14 +60,14 @@ const HomePage = () => {
           <h2 style={styles.h2}>Popular Movies</h2>
           <Button
             component={Link}
-            to="/movies"
+            to="/movie"
             variant="contained"
             color="primary"
             style={styles.button}>
             MORE
           </Button>
         </div>
-        <MovieList movies={popularMovies} />
+        <MovieList movies={popularMovies} type="movie" />
       </div>
 
       <div className="section" style={{ marginBottom: '20px' }}>
@@ -89,14 +75,14 @@ const HomePage = () => {
           <h2 style={styles.h2}>Top Rated Movies</h2>
           <Button
             component={Link}
-            to="/movies/top-rated"
+            to="/movie/top-rated"
             variant="contained"
             color="primary"
             style={styles.button}>
             MORE
           </Button>
         </div>
-        <MovieList movies={topRatedMovies} />
+        <MovieList movies={topRatedMovies} type="movie" />
       </div>
 
       <div className="section" style={{ marginBottom: '20px' }}>
@@ -104,14 +90,14 @@ const HomePage = () => {
           <h2 style={styles.h2}>Upcoming Movies</h2>
           <Button
             component={Link}
-            to="/movies/top-rated"
+            to="/movie/top-rated"
             variant="contained"
             color="primary"
             style={styles.button}>
             MORE
           </Button>
         </div>
-        <MovieList movies={upcoimgMovies} />
+        <MovieList movies={upcoimgMovies} type="movie" />
       </div>
 
       <div className="section" style={{ marginBottom: '20px' }}>
@@ -126,7 +112,7 @@ const HomePage = () => {
             MORE
           </Button>
         </div>
-        <MovieList movies={popularTV} />
+        <MovieList movies={popularTV} type="tv" />
       </div>
     </>
   );
